94e0c189bb648f9bb3c2c488392925450be26101
Daniel Kim <Danielhungkim@gmail.com>
2016-05-04 18:27:47 -0700

    Update linksonly.js

--- linksonly.js
+++ linksonly.js
@@ -3,8 +3,7 @@ var body = $('body');
 var items = $("*");
 $('body').find('*').remove();
 
-
-
+//This will grab all links that have element tag of "A"
 for(var i = 0; i < items.length; i++){
   if(items[i].nodeName === "A"){
     $('body').append(items[i]);
